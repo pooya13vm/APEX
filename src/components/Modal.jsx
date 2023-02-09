2687cad473414b55c131d6917db2caab0746ef94
pooya <pooya13vm@gmail.com>
2023-02-09 19:21:01 +0300

    responsive

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,7 @@
 import { Modal } from "react-bootstrap";
 import "../App.css";
 import pdf from "../assets/documents/apex.pdf";
+import { saveAs } from "file-saver";
 
 const ModalScreen = ({ show, setShow }) => {
   const handleClose = () => setShow(false);
@@ -17,72 +18,27 @@ const ModalScreen = ({ show, setShow }) => {
         textAlign: "center",
       }}
     >
-      <div
-        style={{
-          backgroundColor: "#EEEEEF",
-          textAlign: "center",
-          width: "max-content",
-        }}
-      >
-        <h2
-          style={{
-            marginTop: 50,
-            marginBottom: 25,
-            fontWeight: 800,
-            fontSize: 50,
-            color: "#3a394b",
-          }}
-        >
-          Get More Students Now!
-        </h2>
-        <h4
-          style={{
-            width: "fit-content",
-            marginLeft: 75,
-            marginRight: 75,
-            fontSize: 28,
-            fontWeight: 600,
-            color: "#3a394b",
-          }}
-        >
-          Submit the form below to download the blueprint*
-        </h4>
+      <div className="form-modal-container">
+        <h2>Get More Students Now!</h2>
+        <h4>Submit the form below to download the blueprint*</h4>
 
         <form
           style={{ alignItems: "center" }}
-          id="form"
-          onSubmit={handleSubmit}
+          className="modal-form"
+          // onSubmit={handleSubmit}
+          action="https://formsubmit.co/pooya13vm@gmail.com"
+          method="POST"
         >
           <input
             type="text"
+            name="Full name:"
             placeholder="Full Name*"
             required
-            style={{ width: 300, height: 63, marginTop: 30, paddingLeft: 20 }}
           />
-          <input
-            type="email"
-            placeholder="Email*"
-            required
-            style={{ width: 300, height: 63, marginTop: 10, paddingLeft: 20 }}
-          />
-          <a href={pdf} download="APEX_EDU_BLUEPRINT_23.pdf">
-            <button
-              type="submit"
-              style={{
-                width: 300,
-                height: 63,
-                fontSize: "1.3rem",
-                fontWeight: 700,
-                color: "#fac544",
-                backgroundColor: "#3A394B",
-                border: "none",
-                marginTop: 10,
-                marginBottom: 50,
-              }}
-            >
-              Submit
-            </button>
-          </a>
+          <input type="email" name="Email:" placeholder="Email*" required />
+          {/* <a href={pdf} download="APEX_EDU_BLUEPRINT_23.pdf"> */}
+          <button type="submit">Submit</button>
+          {/* </a> */}
         </form>
       </div>
     </Modal>
